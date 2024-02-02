6afdccfa2f83f1d18002dd93e23c32079ae0d1ff
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-02-02 17:53:50 +0530

    first commit

---
+++ app/(modals)/booking.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const page = () => {
+  return (
+    <View>
+      <Text>Booking</Text>
+    </View>
+  )
+}
+
+export default page
